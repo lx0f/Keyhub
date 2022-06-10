b8c260166b55551fd6129c414f7885ea95b6e5ec
Declan <declanisaackuok@gmail.com>
2022-06-10 22:13:36 +0800

    added google keys

--- src/authentication/passport_google.js
+++ src/authentication/passport_google.js
@@ -1,2 +1,5 @@
 const passport = require("passport")
-const GoogleStrategy = require("passport-google-oidc")
+const GoogleStrategy = require("passport-google-oauth2")
+
+//client id: 70066158066-ea8u4ot65lj1m73i13brakk4sbc4oh6t.apps.googleusercontent.com
+//Secret: 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
