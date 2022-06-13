4f6efa104a4468aa08e20aab6be3c65279e37a71
Declan <declanisaackuok@gmail.com>
2022-06-13 09:35:41 +0800

    added tokens

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,20 @@
 const bcrypt = require("bcrypt");
 const Sequelize = require("sequelize");
 const sequelize = require("./database_setup");
+const moment = require("moment");
 
 class User extends Sequelize.Model {
   compareHash(value) {
     return bcrypt.compareSync(value, this.getDataValue("password"));
+  }
+
+  verifyTokenAge() {
+    return moment().format("x") - this.getDataValue("resetTokenDate")  > 3000000
+  }
+
+  generateResetToken() {
+    this.setDataValue("resetToken", Sequelize.UUIDV4);
+    this.setDataValue("resetTokenDate", `${moment().format("x")}`);
   }
 }
 
@@ -29,24 +39,34 @@ User.init(
     password: {
       type: Sequelize.DataTypes.STRING,
       allowNull: true,
-      unique: false,   
+      unique: false,
       set(value) {
-        if(value) {
-        this.setDataValue("password", bcrypt.hashSync(value, 10) + "");
+        if (value) {
+          this.setDataValue("password", bcrypt.hashSync(value, 10) + "");
         }
-
       },
     },
     isStaff: {
       type: Sequelize.DataTypes.NUMBER,
       allowNull: false,
-      defaultValue: 0
+      defaultValue: 0,
     },
     authMethod: {
       type: Sequelize.DataTypes.STRING,
       allowNull: false,
       defaultValue: "local",
-      isIn: [["local", "oauth"]]
+      isIn: [["local", "oauth"]],
+    },
+    resetTokenID: {
+      //denormalisation
+      type: Sequelize.DataTypes.UUID,
+      allowNull: true,
+      defaultValue: null,
+    },
+    resetTokenDate: {
+      type: Sequelize.DataTypes.NUMBER,
+      allowNull: true,
+      defaultValue: null,
     },
     updatedAt: {
       type: Sequelize.DataTypes.DATE,
@@ -61,7 +81,7 @@ User.init(
     freezeTableName: true,
     timestamps: true,
     sequelize,
-    modelName: "User"
+    modelName: "User",
   }
 );
 
