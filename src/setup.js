5d3b55bf6203bb3d75fb3303624eaa533f2651a1
andy <luthandyka.business@gmail.com>
2022-06-15 19:00:15 +0800

    Added Ticket Admin views
    views
    - all
    - individual
    
    setup.js
    - added format date time helper

--- src/setup.js
+++ src/setup.js
@@ -1,49 +1,48 @@
 //Library Imports
 const express = require("express");
-const flash = require("connect-flash")
+const flash = require("connect-flash");
 const path = require("path");
-const session = require("express-session")
+const session = require("express-session");
 const handlebars = require("handlebars");
+const moment = require("moment");
 const { engine } = require("express-handlebars");
 const bodyParser = require("body-parser");
 const {
-  allowInsecurePrototypeAccess,
+    allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
-const passport = require("passport")
-const methodOverride = require("method-override")
-const cookieParser = require("cookie-parser")
-
+const passport = require("passport");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
 
 //Local Imports
-const initaliseDatabase = require("./models/initalise_database")
-const initalisePassportLocal = require("./authentication/passport_local")
-const initalisePassportAnonymous = require("./authentication/passport_anonymous")
-const InitaliseGoogleLogin = require("./authentication/passport_google")
+const initaliseDatabase = require("./models/initalise_database");
+const initalisePassportLocal = require("./authentication/passport_local");
+const initalisePassportAnonymous = require("./authentication/passport_anonymous");
+const InitaliseGoogleLogin = require("./authentication/passport_google");
 const customerRouter = require("./routes/customer");
 const staffRouter = require("./routes/staff");
 const loginRouter = require("./routes/login");
 
-
-
 //Initialisation of the app
 const app = express();
 
-
 //Setup
 
-app.use(session({
-  secret:"keyhub",
-  rolling: true,
-  cookie: {
-    maxAge: 99999999
-  },
-  saveUninitialized: true,
-  resave: false
-}))
+app.use(
+    session({
+        secret: "keyhub",
+        rolling: true,
+        cookie: {
+            maxAge: 99999999,
+        },
+        saveUninitialized: true,
+        resave: false,
+    })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(passport.session())
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -51,69 +50,69 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(session({
-  secret:"keyhub",
-  resave: false,
-  saveUninitialized: true,
-}))
+app.use(
+    session({
+        secret: "keyhub",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === "object" && "method" in req.body) {
+            // look in urlencoded POST bodies and delete it
+            const method = req.body.method;
+            delete req.body.method;
+            return method;
+        }
+    })
+);
 
-
-app.use(methodOverride(function (req, res) {
-  if (req.body && typeof req.body === 'object' && 'method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    const method = req.body.method
-    delete req.body.method
-    return method
-  }
-}))
-
-app.use(flash())
-
+app.use(flash());
 app.set("view engine", "handlebars");
 
 app.set("views", path.join(__dirname, "../views"));
 
 app.engine(
-  "handlebars",
-  engine({
-    handlebars: allowInsecurePrototypeAccess(handlebars),
-    defaultLayout: "page-layout",
-    helpers: {
-      equals(arg1, arg2, options) {
-        return arg1 == arg2 ? options.fn(this) : options.inverse(this);
-      },
-
-    },
-  })
+    "handlebars",
+    engine({
+        handlebars: allowInsecurePrototypeAccess(handlebars),
+        defaultLayout: "page-layout",
+        helpers: {
+            equals(arg1, arg2, options) {
+                return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+            },
+            dateFormat(date, option) {
+                return moment(date).format(option);
+            },
+        },
+    })
 );
 
 //Initalisation of the database
-initaliseDatabase()
+initaliseDatabase();
 
 //Initalisation of the passport authentication systems
-initalisePassportLocal()
-initalisePassportAnonymous()
-InitaliseGoogleLogin()
-
-
+initalisePassportLocal();
+initalisePassportAnonymous();
+InitaliseGoogleLogin();
 
 //Global variables (middleware)
 app.use((req, res, next) => {
-  res.locals.error = req.flash("error")
-  res.locals.info = req.flash("info")
-  res.locals.success = req.flash("success")
-  res.locals.authenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  res.locals.method = req.body.method
-  next()
-})
+    res.locals.error = req.flash("error");
+    res.locals.info = req.flash("info");
+    res.locals.success = req.flash("success");
+    res.locals.authenticated = req.isAuthenticated();
+    res.locals.user = req.user;
+    res.locals.method = req.body.method;
+    next();
+});
 
 //Routers
 app.use("/staff", staffRouter);
 app.use("/", loginRouter);
 app.use("/", customerRouter);
-
 
 //Export to app.js
 module.exports = app;
