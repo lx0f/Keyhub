adc154ca5a9e2f6f006649e0bd175921db477be8
andy <luthandyka.business@gmail.com>
2022-06-15 22:30:51 +0800

    Added Ticket Comments & Enhance Ticket Table
    - Created TicketComment table
    - Added comment handling & rendering
        - Basic comments
        - Ticket actions (close, reopen) through `meta` attribute
    - Colored ticket status

--- src/routes/manage_tickets.js
+++ src/routes/manage_tickets.js
@@ -2,6 +2,7 @@ const express = require("express");
 const manageTicketRoute = express.Router();
 
 const Ticket = require("../models/Ticket");
+const TicketComment = require("../models/TicketComment");
 const TicketAssignee = require("../models/TicketAssignee");
 const User = require("../models/User");
 
@@ -12,17 +13,36 @@ manageTicketRoute.get("/", async (req, res) => {
 
 manageTicketRoute.patch("/", async (req, res) => {
     const ticket = await Ticket.findByPk(req.body.id);
-    ticket.status = req.body.status;
-    await ticket.save();
-    req.flash("success", "Ticket status changed");
-    res.redirect("/staff/tickets");
+    const meta = req.body.meta;
+    const message = req.body.message;
+
+    ticket.status = meta || ticket.status;
+    ticket.save();
+    await TicketComment.create({
+        message,
+        meta: meta || null,
+        authorID: req.user.id,
+        ticketID: ticket.id,
+    });
+
+    res.redirect(`/staff/tickets/${ticket.id}`);
 });
 
 manageTicketRoute.get("/:id", async (req, res) => {
     const id = req.params.id;
+    const user = req.user;
     const ticket = await Ticket.findByPk(id, { include: User });
+    const comments = await TicketComment.findAll({
+        where: { ticketID: ticket.id },
+        include: { model: User },
+        order: [["createdAt", "ASC"]], // order ascending by creation date
+    });
     if (ticket) {
-        return res.render("./staff/staff-manage-ticket", { ticket });
+        return res.render("./staff/staff-manage-ticket", {
+            ticket,
+            comments,
+            user,
+        });
     }
     req.flash("error", "No such account!");
     return res.redirect("/staff");
