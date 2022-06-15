8decf3fdac43a7075d50d75fce0126168d17b741
NightmareWaifu <irazachary@gmail.com>
2022-06-16 03:07:49 +0800

    goodnight

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const db = require("../models/database_setup")
+const db = require("../models/database_setup");
+const Products = require("../models/product");
 const productRouter = express.Router()
 const product = require("../models/product")
 
@@ -12,13 +13,20 @@ productRouter.post('/', async function (req, res) {
     const productID = await product.count()
     product.create({
         name,description,category,stock,price,productID
-        //list of attributes(not finished)
+        //list of attributes
     })
 
     req.flash("success",name," has been successfully added!")
     req.flash("success",name,description,category,stock,price,"ID:",productID)
     res.redirect("/staff/product")
     });
+
+productRouter.get('/delete', async function (req, res) {
+    let { productID } = req.body;
+    console.log("I AM HERE",productID)
+    const removeProduct = await Products.destroy({ where: {productID: productID}})
+});
+
 productRouter.get('/check',async (req,res)=>{
     const products =  await (await product.findAll()).map((x) => x.dataValues);
     return res.render("./staff/staff-productCheck",{ products });
