16ed1f47bfc10e384d08039c3a0ef9f17fe9070c
NightmareWaifu <irazachary@gmail.com>
2022-06-15 18:55:58 +0800

    yes
    wow routing of product creation and adding to database poggers

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,5 +7,14 @@ productRouter.get('/', (req, res) => {
     console.log("Product Render")
     res.render('./staff/staff-productCreate');
 });
+productRouter.post('/', async function (req, res) {
+    let { test } = req.body;
+    product.create({
+        //list of attributes(not finished)
+    })
+    const input = test
+    req.flash("success",input," has been successfully added!")
+    res.redirect("/staff/product")
+    });
 
 module.exports = productRouter
