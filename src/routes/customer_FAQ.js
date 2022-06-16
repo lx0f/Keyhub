4152372082fc3a0a70eaf825957b3a51deb4cfc5
Declan <declanisaackuok@gmail.com>
2022-06-16 15:14:24 +0800

    bug-fix/added-faq-model

--- src/routes/customer_FAQ.js
+++ src/routes/customer_FAQ.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const FAQrouter = express.Router()
+const FAQs = require("../models/FAQs")
 
 FAQrouter.route("/").get(async (req, res) => {
     const faqs = await (await FAQs.findAll()).map((x) => x.dataValues);
