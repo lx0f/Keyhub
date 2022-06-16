038a5c2272da5778221ba9a01123dee45042654b
Declan <declanisaackuok@gmail.com>
2022-06-16 15:13:15 +0800

    bug-fix/added-async-func-for-await

--- src/routes/customer_FAQ.js
+++ src/routes/customer_FAQ.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const FAQrouter = express.Router()
 
-FAQrouter.route("/").get((req, res) => {
+FAQrouter.route("/").get(async (req, res) => {
     const faqs = await (await FAQs.findAll()).map((x) => x.dataValues);
     return res.render("./customers/page-faqs", { faqs });
 })
