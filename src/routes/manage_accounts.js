4ffeb440b2d5cb6556d195042e4d5aec91bd1db6
andy <luthandyka.business@gmail.com>
2022-06-18 00:48:32 +0800

    Added routes to update Ticket attributes
    - Search and assign staff
    - Reassign category
    - Reassign severity

--- src/routes/manage_accounts.js
+++ src/routes/manage_accounts.js
@@ -1,42 +1,61 @@
 const express = require("express");
 const User = require("../models/User");
 const manageAccountRoute = express.Router();
+const { Op } = require("sequelize");
 
 manageAccountRoute
-  .route("/")
-  .get(async (req, res) => {
-    const users = await (await User.findAll()).map((x) => x.dataValues);
-    return res.render("./staff/staff-tables", { users });
-  })
-  .delete(async (req, res) => {
-    await User.destroy({ where: { id: req.body.id } });
-    req.flash("error", "Account has been deleted");
-    res.redirect("/staff/accounts");
-  })
-  .patch(async (req, res) => {
-    const user = await User.findByPk(req.body.id);
-    user.isStaff = req.body.isStaff || user.isStaff;
-    user.username = req.body.username || user.username;
-    user.email = req.body.email || user.email;
-    if (req.body.password) {
-      user.password = req.body.password; //unable to use short circuit eval as hashed password
-    }
+    .route("/")
+    .get(async (req, res) => {
+        const users = await (await User.findAll()).map((x) => x.dataValues);
+        return res.render("./staff/staff-tables", { users });
+    })
+    .delete(async (req, res) => {
+        await User.destroy({ where: { id: req.body.id } });
+        req.flash("error", "Account has been deleted");
+        res.redirect("/staff/accounts");
+    })
+    .patch(async (req, res) => {
+        const user = await User.findByPk(req.body.id);
+        user.isStaff = req.body.isStaff || user.isStaff;
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password; //unable to use short circuit eval as hashed password
+        }
 
-    await user.save();
-    req.flash("success", "User updated!");
-    res.redirect("/staff/accounts");
-  });
+        await user.save();
+        req.flash("success", "User updated!");
+        res.redirect("/staff/accounts");
+    });
+
+// Author: @lx0f
+// To get staff usernames and id when
+// querying and assigning users to a ticket
+// Refer to route /staff/tickets/:id
+manageAccountRoute.route("/users/:query").get(async (req, res) => {
+    const query = req.params.query;
+    const users = await User.findAll({
+        attributes: ["id", "username"],
+        where: {
+            username: {
+                [Op.regexp]: query,
+            },
+            isStaff: 1, // 1 == truthy
+        },
+    });
+    return res.send({ users });
+});
 
 manageAccountRoute.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const user = await User.findByPk(id);
-  if (user) {
-    return res.render("./staff/staff-manage-account", {
-      user: user.dataValues, //what happens when no id
-    });
-  }
-  req.flash("error", "No such account!");
-  return res.redirect("/staff");
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    if (user) {
+        return res.render("./staff/staff-manage-account", {
+            user: user.dataValues, //what happens when no id
+        });
+    }
+    req.flash("error", "No such account!");
+    return res.redirect("/staff");
 });
 
 module.exports = manageAccountRoute;
--- src/routes/manage_tickets.js
+++ src/routes/manage_tickets.js
@@ -28,6 +28,96 @@ manageTicketRoute.patch("/", async (req, res) => {
     res.redirect(`/staff/tickets/${ticket.id}`);
 });
 
+manageTicketRoute.post("/assign/user", async (req, res) => {
+    const userID = req.body.user;
+    const ticketID = req.body.id;
+    console.log(userID);
+
+    const ticketAssignee = await TicketAssignee.findOne({
+        where: { userID, ticketID },
+    });
+
+    if (ticketAssignee) {
+        req.flash("error", `User already assigned to ticket ${ticketID}.`);
+        return res.redirect(`/staff/tickets/${ticketID}`);
+    }
+
+    await TicketAssignee.create({
+        ticketID,
+        userID,
+    });
+
+    return res.redirect(`/staff/tickets/${ticketID}`);
+});
+
+manageTicketRoute.post("/reassign/user", async (req, res) => {
+    const ticket = await Ticket.findByPk(req.body.id);
+    const assignees = await TicketAssignee.findAll({
+        where: { ticketID: ticket.id },
+        include: { model: User },
+    });
+    const userIDs = req.body.users;
+    const assigneeIDs = assignees.map((assignee) => assignee.id);
+
+    console.log(userIDs);
+
+    // add new assignees
+    if (userIDs) {
+        userIDs.forEach(async (userID) => {
+            if (!assigneeIDs.includes(userID)) {
+                await TicketAssignee.create({
+                    ticketID: ticket.id,
+                    userID: userID,
+                });
+            }
+        });
+    }
+
+    // delete unassigned assignees
+    if (assigneeIDs) {
+        assigneeIDs.forEach(async (assigneeID) => {
+            if (userIDs) {
+                if (!userIDs.includes(assigneeID)) {
+                    const assignee = await TicketAssignee.findOne({
+                        where: {
+                            ticketID: ticket.id,
+                            userID: assigneeID,
+                        },
+                    });
+                    await TicketAssignee.destroy(assignee);
+                }
+            } else {
+                const assignee = await TicketAssignee.findOne({
+                    where: {
+                        ticketID: ticket.id,
+                    },
+                });
+                if (assignee) {
+                    await assignee.destroy();
+                }
+            }
+        });
+    }
+
+    return res.redirect(`/staff/tickets/${ticket.id}`);
+});
+
+manageTicketRoute.post("/assign/category", async (req, res) => {
+    const ticketID = req.body.id;
+    const category = req.body.category;
+    const ticket = await Ticket.findByPk(ticketID);
+    ticket.update({ category });
+    return res.redirect(`/staff/tickets/${ticket.id}`);
+});
+
+manageTicketRoute.post("/assign/severity", async (req, res) => {
+    const ticketID = req.body.id;
+    const severity = req.body.severity;
+    const ticket = await Ticket.findByPk(ticketID);
+    ticket.update({ severity });
+    return res.redirect(`/staff/tickets/${ticket.id}`);
+});
+
 manageTicketRoute.get("/:id", async (req, res) => {
     const id = req.params.id;
     const user = req.user;
@@ -37,11 +127,16 @@ manageTicketRoute.get("/:id", async (req, res) => {
         include: { model: User },
         order: [["createdAt", "ASC"]], // order ascending by creation date
     });
+    const assignees = await TicketAssignee.findAll({
+        where: { ticketID: ticket.id },
+        include: { model: User },
+    });
     if (ticket) {
         return res.render("./staff/staff-manage-ticket", {
             ticket,
             comments,
             user,
+            assignees,
         });
     }
     req.flash("error", "No such account!");
