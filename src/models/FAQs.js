64cfd3b5034d7ba528bdd1acf904177490e182f6
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2022-06-21 00:55:00 +0800

    ...

--- src/models/FAQs.js
+++ src/models/FAQs.js
@@ -1,10 +1,46 @@
 const Sequelize = require('sequelize');
-const db = require('./database_setup');
+const sequelize = require('./database_setup');
 
-const FAQs = db.define('Faqs',
-{
-    Question: { type: Sequelize.STRING },
-    Answer: { type: Sequelize.STRING },
-});
-
-module.exports = FAQs;
+class FAQs extends Sequelize.Model {
+    
+  }
+  
+  FAQs.init(
+    {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+      },
+      Category:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Question:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Answer: {
+        type:Sequelize.STRING,
+        allowNull:false,
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      }
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+      sequelize,
+      modelName: "FAQs"
+    }
+  );
+  
+  module.exports = FAQs;
--- src/models/product_evaluation.js
+++ src/models/product_evaluation.js
@@ -1,12 +1,50 @@
 const Sequelize = require('sequelize');
-const db = require('./database_setup');
+const sequelize = require('./database_setup');
 
-const Pevaluation = db.define('product evaluation',
-{
-    ProductName: { type: Sequelize.STRING },
-    ProductCategory: { type: Sequelize.STRING },
-    ProductRating: { type: Sequelize.INTEGER },
-    ProductRemarks: { type: Sequelize.STRING },
-});
-
-module.exports = Pevaluation;
+class Pevaluation extends Sequelize.Model {
+    
+  }
+  
+  Pevaluation.init(
+    {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+      },
+      ProductName:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ProductCategory: {
+        type:Sequelize.STRING,
+        allowNull:false,
+      },
+      ProductRating: {
+        type:Sequelize.INTEGER,
+        allowNull:false,
+      },
+      ProductRemarks: {
+        type:Sequelize.STRING,
+        allowNull:false,
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      }
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+      sequelize,
+      modelName: "Product_Evaluation"
+    }
+  );
+  
+  module.exports = Pevaluation;
