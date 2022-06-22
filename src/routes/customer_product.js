36d75f6a26efcd0f23019dc8c708af82f768ef69
NightmareWaifu <irazachary@gmail.com>
2022-06-23 02:02:57 +0800

    categories for products
    icons stopped loading someone help

--- src/routes/customer_product.js
+++ src/routes/customer_product.js
@@ -3,9 +3,61 @@ const productRouter = express.Router()
 const product = require("../models/product")
 
 productRouter.route('/general').get(async(req,res)=>{
-    const items = await product.count()
     const products = await (await product.findAll()).map((x) => x.dataValues);
-    return res.render("./customers/page-listing-grid",{products,items});
+    const display = products
+    const items = await display.length
+    return res.render("./customers/page-listing-grid",{display,items});
 })
 
+productRouter.get('/keyboards', async(req,res)=>{
+    const products = await (await product.findAll()).map((x) => x.dataValues);
+    const display = []
+    for (let index = 0; index < products.length; index++) {
+        if (products[index]["category"]=="keyboard"){
+            display.push(products[index])
+        }
+        
+    }
+    const items = await display.length
+    res.render("./customers/page-listing-grid",{display,items});
+})
+
+productRouter.get('/switches', async(req,res)=>{
+    const products = await (await product.findAll()).map((x) => x.dataValues);
+    const display = []
+    for (let index = 0; index < products.length; index++) {
+        if (products[index]["category"]=="switch"){
+            display.push(products[index])
+        }
+        
+    }
+    const items = await display.length
+    res.render("./customers/page-listing-grid",{display,items});
+})
+
+productRouter.get('/keycaps', async(req,res)=>{
+    const products = await (await product.findAll()).map((x) => x.dataValues);
+    const display = []
+    for (let index = 0; index < products.length; index++) {
+        if (products[index]["category"]=="keycaps"){
+            display.push(products[index])
+        }
+        
+    }
+    const items = await display.length
+    res.render("./customers/page-listing-grid",{display,items});
+})
+
+productRouter.get('/others', async(req,res)=>{
+    const products = await (await product.findAll()).map((x) => x.dataValues);
+    const display = []
+    for (let index = 0; index < products.length; index++) {
+        if (products[index]["category"]=="others"){
+            display.push(products[index])
+        }
+        
+    }
+    const items = await display.length
+    res.render("./customers/page-listing-grid",{display,items});
+})
 module.exports = productRouter
