1b899fedf9c9e8ae14de919725abc4bdb2777e38
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2022-06-27 19:04:15 +0800

    add to cart CRUD done

--- src/routes/shoppingcart.js
+++ src/routes/shoppingcart.js
@@ -93,26 +93,40 @@ ShoppingCart.post('/:productId/add', async (req,res) =>{
         // check product quantity+1 > inventory or not
         if (product.quantity + 1 > addProduct.stock) {
           req.flash('error', `ProductId:${product.ProductId} stocks left with ${addProduct.stock}`)
-          return res.redirect('back')
+          // return res.redirect('back')
         }
         await product.update({
           quantity: product.quantity + 1
         })
-        // return res.status(200).redirect('back')
+        return res.redirect('/cart')
       } catch (e) {
         console.log(e)
-        // return next(e)
       }
+});
+
+// subCartItem
+ShoppingCart.post('/:productId/sub', async (req,res) =>{
+  try {
+    // find cart
+    const product = await CartItem.findByPk(req.params.productId)
+    await product.update({
+      quantity: product.quantity - 1 ? product.quantity - 1 : 1
+    })
+    return res.status(200).redirect('back')
+  } catch (e) {
+    console.log(e)
+  }
 });
 
 
 // Delete Cart Item
-ShoppingCart.delete('/:productId', async (req,res) =>{
+ShoppingCart.get('/:productId', async (req,res) =>{
     try {
         // find cart
         const product = await CartItem.findByPk(req.params.productId)
         await product.destroy()
-        // return res.status(200).redirect('back')
+        req.flash("success", "Successuflly deleted");
+        return res.redirect('/cart')
       } catch (e) {
         console.log(e)
         // return next(e)
