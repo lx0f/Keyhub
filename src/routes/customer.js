d5bc4cde06fcf9fe1af170408709d31fb027a763
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2022-06-29 00:18:35 +0800

    ...

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -9,6 +9,7 @@ const customerproductRouter = require("./customer_product")
 const customerManageAccountRouter = require("./customer_manage_account")
 const ShoppingCart = require("./shoppingcart");
 const CustomerOrder = require("./order");
+
 /*customerRouter.use((req, res, next) => {
   if (!req.isAuthenticated()) {
       return res.redirect("/login")
@@ -31,6 +32,7 @@ customerRouter.use("/ticket", customerTicketRouter);
 customerRouter.use("/createPE", customerpeRouter)
 customerRouter.use("/Cproducts",customerproductRouter)
 customerRouter.use("/account", customerManageAccountRouter)
+
 customerRouter.use("/cart",ShoppingCart)
 customerRouter.use("/order",CustomerOrder)
 
