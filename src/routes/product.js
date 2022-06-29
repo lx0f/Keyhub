3858626edf93d3577d5443de653af7fa76e10c0c
NightmareWaifu <irazachary@gmail.com>
2022-06-29 23:46:27 +0800

    a comment

--- src/routes/product.js
+++ src/routes/product.js
@@ -84,7 +84,7 @@ productRouter.post('/update',async function(req,res){
         stock: stock,
         price: price
     },
-        {where: {id: id}}
+        {where: {id: id}}//change the button value to this.name to use name:id comparison
     )
     //req.flash("success",name," has been updated successfully!")
     //res.render("./staff/staff-productCheck",{ products });
