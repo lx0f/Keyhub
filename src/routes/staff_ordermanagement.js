9182270c52b907447c011cd77d672e574349eb3b
andy <luthandyka.business@gmail.com>
2022-07-11 11:59:42 +0800

    Fixed admin order table bug
    - Changed Order-Product association to manual instead of `through:
      'orderProducts'`
    - Simplied Order, OrderItem, and Product query
    - Fixed datatable format
    - Added new modal to view products in a order item as a table. Displays:
        - id
        - name
        - unit price
        - total price
        - quantity

--- src/routes/staff_ordermanagement.js
+++ src/routes/staff_ordermanagement.js
@@ -1,43 +1,23 @@
 const express = require("express")
 const OrderManagement = express.Router()
 const { Order }  = require("../models/order")
-const {OrderItem} = require("../models/order")
+const { OrderItem } = require("../models/order")
 const Product = require("../models/product")
-const {Payment} = require("../models/order")
-const {Cart} = require("../models/cart")
+const { Payment } = require("../models/order")
+const { Cart } = require("../models/cart")
 
 // Get Orders
 OrderManagement.get('/', async (req, res) => {
-    try {
-        const ordersHavingProducts = await Order.findAll({
-          raw: true,
-          nest: true,
-          include: 'orderProducts'
-        })
-        console.log(ordersHavingProducts)
-        const orders = await Order.findAll({
-          raw: true,
-          nest: true,
-        })
-
-        orders.forEach(order => {
-          order.orderProducts = []
-        })
-
-        ordersHavingProducts.forEach(product => {
-          const index = orders.findIndex(order => order.id === product.id)
-          console.log(index)
-          if (index === -1) 
-            return orders[index].orderProducts.push(product.orderProducts)
-        })
-        // const orders = await Order.findAll({
-        //     include: "orderProducts"
-        //   })
-        
-        return res.render('./staff/ordermanagement/staff-getorders', { orders })
-      } catch (e) {
-        console.log(e)
-      }
+    const orders = await Order.findAll({
+        include: {
+            model: OrderItem,
+            include: {
+                model: Product
+            }
+        },
+    });
+    console.log(orders)
+    return res.render('./staff/ordermanagement/staff-getorders', { orders });
 });
 
 module.exports = OrderManagement
