b6b548cb5f34c823f848bec12e40d61391501c27
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2022-07-19 18:31:55 +0800

    ordermanagementviewdetails

--- src/routes/staff_ordermanagement.js
+++ src/routes/staff_ordermanagement.js
@@ -5,18 +5,29 @@ const { OrderItem } = require("../models/order")
 const Product = require("../models/product")
 const { Payment } = require("../models/order")
 const { Cart } = require("../models/cart")
+const User = require("../models/User")
+const moment = require('moment');
+
 
 // Get Orders
 OrderManagement.get('/', async (req, res) => {
     const orders = await Order.findAll({
-        include: {
-            model: OrderItem,
-            include: {
-                model: Product
-            }
-        },
+        include: [
+            {
+                model: OrderItem,
+                include: {
+                    model: Product
+                }
+            },
+            {
+                model: User
+            },
+        ],
     });
-
+    console.log(orders)
+    // Order
+    // noofitems = orders.OrderItem.length
+    
     
     return res.render('./staff/ordermanagement/staff-getorders', { orders });
 });
