ccbbaac3397d93192b65ef0139ffe39c20ef06ba
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2022-07-22 00:01:49 +0800

    automatically delete order
    if it is unpaid and didnt finish payment in 30mins

--- src/routes/staff_ordermanagement.js
+++ src/routes/staff_ordermanagement.js
@@ -7,7 +7,25 @@ const { Payment } = require("../models/order")
 const { Cart } = require("../models/cart")
 const User = require("../models/User")
 const moment = require('moment');
+const cron = require('node-cron');
 
+cron.schedule('*/30 * * * * *', async() => {
+    console.log('running a task every 30 minute');
+    let orders = await Order.findAll({where : {payment_status: 0 }})
+    orders.forEach(order => {
+        var createAt = moment(order.createdAt).format('YYYY-MM-DD HH:mm:ss')
+        console.log(createAt)
+        var now = moment().format('YYYY-MM-DD HH:mm:ss')
+        console.log(now)
+        var minute = moment(now).diff(moment(createAt),'minutes');
+        console.log(minute)
+        if (minute > 30){
+            Order.destroy({where : {id: order.id }});
+            OrderItem.destroy({where : {orderid: order.id }});
+            console.log("Hello");
+        }
+  });
+});
 
 // Get Orders
 OrderManagement.get('/', async (req, res) => {
@@ -24,12 +42,27 @@ OrderManagement.get('/', async (req, res) => {
             },
         ],
     });
-    console.log(orders)
-    // Order
-    // noofitems = orders.OrderItem.length
-    
-    
+
     return res.render('./staff/ordermanagement/staff-getorders', { orders });
 });
+
+OrderManagement.get('/deleteorder/:id', async function (req, res) {
+    try {
+        let order = await Order.findByPk(req.params.id);
+        if (!order) {
+            flash(res, 'error', 'order not found');
+            res.redirect('/staff/manage_order');
+            return;
+        }
+        let result = await Order.destroy({ where: { id: order.id } });
+        OrderItem.destroy({where : {orderid: order.id }})
+        req.flash("success", "Order" + " is deleted!");
+        res.redirect('/staff/manage-orders');
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
 
 module.exports = OrderManagement
