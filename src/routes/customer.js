3a6e4fbb047a97b1e20ea60c7b5566beea1c8c0a
Zhe-Yin <tayzheyin123@gmail.com>
2022-07-27 20:31:29 +0800

    Voucher
    small update

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -4,9 +4,9 @@ const customerRouter = express.Router();
 const customerFAQRouter = require("./customer_FAQ");
 const customerTicketRouter = require("./customer_tickets");
 
-const customerpeRouter = require("./customer_pe")
-const customerproductRouter = require("./customer_product")
-const customerManageAccountRouter = require("./customer_manage_account")
+const customerpeRouter = require("./customer_pe");
+const customerproductRouter = require("./customer_product");
+const customerManageAccountRouter = require("./customer_manage_account");
 
 const ShoppingCart = require("./shoppingcart");
 const CustomerOrder = require("./order");
@@ -24,32 +24,29 @@ const CustomerVoucher = require("./customer_voucher");
 /** FAQs Customer Site**/
 
 customerRouter.use((req, res, next) => {
-    res.locals.path = req.baseUrl;
-    console.log(req.baseUrl);
-    next();
+  res.locals.path = req.baseUrl;
+  console.log(req.baseUrl);
+  next();
 });
-
 
 customerRouter.use("/faqs", customerFAQRouter);
 customerRouter.use("/ticket", customerTicketRouter);
-customerRouter.use("/createPE", customerpeRouter)
-customerRouter.use("/Cproducts",customerproductRouter)
-customerRouter.use("/account", customerManageAccountRouter)
+customerRouter.use("/createPE", customerpeRouter);
+customerRouter.use("/Cproducts", customerproductRouter);
+customerRouter.use("/account", customerManageAccountRouter);
 
-
-customerRouter.use("/cart",ShoppingCart)
-customerRouter.use("/order", CustomerOrder)
+customerRouter.use("/cart", ShoppingCart);
+customerRouter.use("/order", CustomerOrder);
 
 customerRouter.use("/CustomerVoucher", CustomerVoucher);
 
-
 customerRouter.route("/logout").get((req, res) => {
-    req.logOut();
-    res.redirect("/login");
+  req.logOut();
+  res.redirect("/login");
 });
 
 customerRouter.route("/").get((req, res) => {
-    res.render("./customers/page-index-3");
+  res.render("./customers/page-index-3");
 });
 
 module.exports = customerRouter;
