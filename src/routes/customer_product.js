6ec0ef2005694e603a3be02c96699c44a1b75b6c
NightmareWaifu <irazachary@gmail.com>
2022-07-29 14:36:01 +0800

    optimized search bar

--- src/routes/customer_product.js
+++ src/routes/customer_product.js
@@ -15,12 +15,25 @@ productRouter.post("/search",async function(req,res){
     const display = []
   
     for (let index = 0; index < products.length; index++) {
-        if (products[index]["name"]==search){
+        // if (products[index]["name"]==search){
+        //     display.push(products[index])
+        // }
+        var match = true;
+        for (let letter = 0; letter < search.length; letter++) {
+            if (products[index]["name"][letter].toUpperCase() == search[letter].toUpperCase()){
+                match = true;
+            }
+            else{
+                match = false;
+                break;
+            }
+        }
+        if (match==true) {
             display.push(products[index])
         }
     }
     const items = await display.length
-    return res.render("./customers/page-listing-grid",{display,items});
+    return res.render("./customers/page-listing-grid",{display,items,search});
     
 })
 
@@ -28,8 +41,9 @@ productRouter.route('/general').get(async(req,res)=>{
     const products = await (await product.findAll()).map((x) => x.dataValues);
     const display = products
     const items = await display.length
+    const search = null
     //var image
-    return res.render("./customers/page-listing-grid",{display,items});
+    return res.render("./customers/page-listing-grid",{display,items,search});
 })
 
 productRouter.get('/keyboards', async(req,res)=>{
