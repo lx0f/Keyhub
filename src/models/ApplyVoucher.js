f1fff022c56d68f1267159aaa0eb50de573230d3
Zhe-Yin <tayzheyin123@gmail.com>
2022-08-06 18:23:08 +0800

    apply

---
+++ src/models/ApplyVoucher.js
@@ -0,0 +1,47 @@
+const Sequelize = require("sequelize");
+const sequelize = require("./database_setup");
+const Voucher = require("./Voucher");
+const User = require("./User");
+
+
+class ApplyVoucher extends Sequelize.Model {}
+
+ApplyVoucher.init(
+    {
+        id:{
+            type: Sequelize.DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            unique: true,
+        },
+        VoucherId:{
+            type: Sequelize.DataTypes.INTEGER,
+        },
+        UserId: {
+            type: Sequelize.DataTypes.STRING,
+        }
+    },
+    {
+        freezeTableName: true,
+        timestamps: true,
+        sequelize,
+        modelName: "ApplyVoucher",
+    }
+);
+
+
+
+
+ApplyVoucher.belongsTo(Voucher, {
+   
+    foreignKey: 'VoucherId',
+    as:"voucher"
+   
+  })
+
+Voucher.hasMany(ApplyVoucher, {
+    foreignKey: 'ApplyVoucherId',
+    as: 'applyvoucher'
+});
+  
+module.exports = ApplyVoucher;
